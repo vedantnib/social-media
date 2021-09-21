1ec29bac1b6250134088dd2d8464178e591c7889
Vedant Nibandhe <vnibandhe@gmail.com>
2021-09-21 03:10:00 -0500

    feat: completed auth apis sigin and signout

--- server/express.js
+++ server/express.js
@@ -35,6 +35,15 @@ app.get('/',(req, res) => {
 
 app.use('/', userRoutes);
 app.use('/', authRoutes);
+app.use((err, req, res, next) => {
+  if (err.name === 'UnauthorizedError') {
+    res.status(401).json({"error": err.name + ": " + err.message})
+  }
+  else if (err) {
+    res.status(400).json({"error": err.name + ": "+ err.message})
+    console.log(err)
+  }
+})
 
 // Catch unauthorised errors
 
