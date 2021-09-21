6a46699bf21bdc0bb6fbfb9204576a9c67d20653
Vedant Nibandhe <vnibandhe@gmail.com>
2021-09-21 02:20:38 -0500

    feat: completed the read, update, remove apis

--- server/controllers/user.controller.js
+++ server/controllers/user.controller.js
@@ -46,8 +46,42 @@ const userByID = async (req, res, next, id) => {
     }
 }
 
-const read = (req, res) => { }
-const update = (req, res, next) => { }
-const remove = (req, res, next) => { }
+const read = (req, res) => { 
+    req.profile.hashed_password = undefined
+    req.profile.salt = undefined
+    return res.json(req.profile)
+}
+
+const update = async (req, res, next) => {
+    try {
+        let user = req.profile
+        user = extend(user, req.body)
+        user.updated = Date.now()
+        await user.save()
+        user.hashed_password = undefined
+        user.salt = undefined
+        res.json(user)
+    }
+    catch(error) {
+        return res.status(400).json({
+            error: errorHandler.getErrorMessage(error)
+        })
+    }
+ }
+
+const remove = async (req, res, next) => { 
+    try {
+        let user = req.profile
+        let deletedUser = await user.remove()
+        deletedUser.hashed_password = undefined
+        deletedUser.salt = undefined
+        res.json(deletedUser)
+    }
+    catch(error) {
+        return res.status(400).json({
+            error: errorHandler.getErrorMessage(error)
+        })
+    }
+}
 
 export default { create, userByID, read, list, remove, update }
