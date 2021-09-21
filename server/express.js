9d3fef9e499fb1b519e13e64868bbb08a6738341
Vedant Nibandhe <vnibandhe@gmail.com>
2021-09-21 01:31:51 -0500

    chore: project setup

---
+++ server/express.js
@@ -0,0 +1,39 @@
+import express from 'express'
+import path from 'path'
+import bodyParser from 'body-parser'
+import cookieParser from 'cookie-parser'
+import compress from 'compression'
+import cors from 'cors'
+import helmet from 'helmet'
+import Template from './../template'
+
+// modules for server side rendering
+//end
+
+//comment out before building for production
+import devBundle from './devBundle'
+
+const CURRENT_WORKING_DIR = process.cwd()
+const app = express()
+
+//comment out before building for production
+devBundle.compile(app)
+
+// parse body params and attache them to req.body
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+app.use(cookieParser())
+app.use(compress())
+// secure apps by setting various HTTP headers
+app.use(helmet())
+// enable CORS - Cross Origin Resource Sharing
+app.use(cors())
+app.get('/',(req, res) => {
+  res.status(200).send(Template());
+});
+
+
+
+// Catch unauthorised errors
+
+export default app
